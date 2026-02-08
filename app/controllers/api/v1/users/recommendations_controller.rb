382689d5946e8f520d86d4b04486c5bfb3a8f707
daviddluci <gliaubertasdeividas@gmail.com>
2026-02-09 00:54:34 +0200

    add: 200 controller placeholder

---
+++ app/controllers/api/v1/users/recommendations_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Api::V1::Users::RecommendationsController < ApplicationController
+  def index
+    render status: 200, json: { }
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  GENERIC_EXCEPTION_CODE = 1000
+
+  rescue_from Exception, with: :exception_handler
+
+  private
+
+  def exception_handler(exception)
+    backtrace = Rails.backtrace_cleaner.clean(exception.backtrace)
+
+    if Rails.env.production?
+      render status: :internal_server_error
+    else
+      render json: json(exception, backtrace), status: exception.try(:status) || 500
+    end
+  end
+
+  def json(exception, backtrace)
+    {
+      code: exception.try(:code) || GENERIC_EXCEPTION_CODE,
+      message: exception.message,
+      backtrace: backtrace,
+    }
+  end
 end
