bffbdc3a2ef5db2d1f8cd68526071feb5a789f7f
daviddluci <gliaubertasdeividas@gmail.com>
2026-02-20 22:49:13 +0200

    change: routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   namespace :api do
-    namespace :v1 do
-      resources :users, only: [] do
-        resources :recommendations, only: [:index], controller: 'users/recommendations'
-      end
+    resources :users, only: [] do
+      resources :recommendations, only: [:index], controller: 'users/recommendations'
     end
   end
 end
