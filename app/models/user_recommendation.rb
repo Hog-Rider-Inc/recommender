f0f84304c519bb946a2d428ed82ae0ebe6accc07
daviddluci <gliaubertasdeividas@gmail.com>
2026-03-23 19:52:40 +0200

    remove: testini modeli

--- app/models/user_recommendation.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class UserRecommendation < ApplicationRecord
-  scope :recent_for_user, lambda { |user_id, count = 3|
-    where(user_id: user_id).order(created_at: :desc).limit(count)
-  }
-end
